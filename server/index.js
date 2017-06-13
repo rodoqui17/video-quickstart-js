408f19cba7218b4bd70c73bc83c9fa3e7a062d0c
rodoqui17 <quiroz.rodrigo@gmail.com>
2017-06-13 03:32:03 -0400

    Create index.js

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ app.get('/token', function(request, response) {
 
 // Create http server and run it.
 var server = http.createServer(app);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 server.listen(port, function() {
   console.log('Express server running on *:' + port);
 });
